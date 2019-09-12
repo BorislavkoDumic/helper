0e0c4ad5f4a7b611f020aa455b586f3b617623f8
Borislavko Dumic <borislavko.dumic@biologis.com>
2019-09-12 15:47:43 +0200

    Adding functions of buttons, remove some content.

--- src/components_redux/Input.js
+++ src/components_redux/Input.js
@@ -4,14 +4,44 @@ class Input extends Component {
   constructor(props) {
     super(props);
     this.state = {};
-    this.openNav = this.openNav.bind(this);
+    this.openNav = this.openNav;
+    this.state = { values: [] };
+    this.handleSubmit = this.handleSubmit;
   }
-  openNav() {
+  openNav = () => {
     const style = { width: 550 };
     this.setState({ style });
-  }
+  };
   handleChange = () => {
     this.setState({ showComponet: !this.state.showComponet });
+  };
+
+  createUI = () => {
+    return this.state.values.map((el, i) => (
+      <div key={i}>
+        <label>Value</label>
+        <input type="text" />
+        <input type="button" value="DELETE" onClick={this.removeClick} />
+      </div>
+    ));
+  };
+
+  handleChange(i, event) {
+    let values = [...this.state.values];
+    values[i] = event.target.value;
+    this.setState({ values });
+  }
+  addClick = () => {
+    this.setState(prevState => ({ values: [...prevState.values, ""] }));
+  };
+  removeClick = i => {
+    let values = [...this.state.values];
+    values.splice(i, 1);
+    this.setState({ values });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
   };
 
   render() {
@@ -25,20 +55,21 @@ class Input extends Component {
             placeholder="Click here to for input text"
           ></input>
         </div>
-        <div className="overlay" style={this.state.style}>
+        <div className="overlay">
           <h2>Fill the form</h2>
           <label className="label">
             Label:
             <input type="text"></input>
           </label>
           <form className="inputForm">
+            <label>Type</label>
             <label>
               <input
                 type="radio"
                 name="input"
                 onChange={this.handleChange}
                 checked={!this.state.showComponet}
-              />{" "}
+              />
               Textfield
             </label>
             <label>
@@ -47,7 +78,7 @@ class Input extends Component {
                 name="input"
                 onChange={this.handleChange}
                 checked={!this.state.showComponet}
-              />{" "}
+              />
               Checkbox
             </label>
             <label>
@@ -56,7 +87,7 @@ class Input extends Component {
                 name="input"
                 onChange={this.handleChange}
                 checked={this.state.showComponet}
-              />{" "}
+              />
               Select
             </label>
             <label>
@@ -65,20 +96,21 @@ class Input extends Component {
                 name="input"
                 onChange={this.handleChange}
                 checked={this.state.showComponet}
-              />{" "}
+              />
               Radios
             </label>
             {this.state.showComponet ? (
               <div>
-                {" "}
                 <label>ADD MORE OPTIONS</label>
-                <form className="inputForm">
+                <form className="inputForm" onSubmit={this.handleSubmit}>
+                  {this.createUI()}
                   <label>
                     Value
                     <input type="text" name="input" />
                   </label>
-                  <button className="button-small">ADD</button>
-                  <button className="button-small">EDIT</button>
+                  <button className="button-small" onClick={this.addClick}>
+                    ADD
+                  </button>
                   <button className="button-small">DELETE</button>
                 </form>
               </div>
